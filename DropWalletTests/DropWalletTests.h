d2a7c046448f31efd0c6ace450edbbc1de0b3d37
Paul Rudolph <paulrudolph@Jareds-iMac.local>
2012-03-16 09:23:54 -0400

    Initial Commit

---
+++ DropWalletTests/DropWalletTests.h
@@ -0,0 +1,13 @@
+//
+//  DropWalletTests.h
+//  DropWalletTests
+//
+//  Created by Paul Rudolph on 3/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DropWalletTests : SenTestCase
+
+@end
---
+++ DropWalletTests/DropWalletTests.m
@@ -0,0 +1,32 @@
+//
+//  DropWalletTests.m
+//  DropWalletTests
+//
+//  Created by Paul Rudolph on 3/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DropWalletTests.h"
+
+@implementation DropWalletTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DropWalletTests");
+}
+
+@end
